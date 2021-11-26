af1a546fafa131ae72e70c22b8f31bbdca183320
Andrew Bettke <abettke@gmail.com>
2021-11-26 11:00:05 -0500

    Add description to form validators for mutations.

--- app/tasks/mutations/createTask.ts
+++ app/tasks/mutations/createTask.ts
@@ -4,6 +4,7 @@ import { z } from "zod"
 
 const CreateTask = z.object({
   name: z.string(),
+  description: z.string(),
 })
 
 export default resolver.pipe(resolver.zod(CreateTask), resolver.authorize(), async (input) => {
--- app/tasks/mutations/updateTask.ts
+++ app/tasks/mutations/updateTask.ts
@@ -5,6 +5,7 @@ import { z } from "zod"
 const UpdateTask = z.object({
   id: z.number(),
   name: z.string(),
+  description: z.string(),
 })
 
 export default resolver.pipe(
